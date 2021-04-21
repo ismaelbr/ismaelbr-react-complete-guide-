0c18a5817f8ae06d4b38b4a21527f49b077ced06
ismael.guilherme <ismael.guilherme@gmail.com>
2021-04-21 00:24:45 -0300

    feat: single state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2021, 2, 28);
-  // const expenseTitle = 'Car Insurance';
-  // const expenseAmount = '294.67';
-  const month = props.date.toLocaleString('pt-BR', {month: 'long'});
-  const day = props.date.toLocaleString('pt-BR', {day: '2-digit'});
-  const year = props.date.getFullYear();
+  const [title, setTitle] = useState(props.title)
 
-  const expenseTitle = props.title;
+  // let expenseTitle = props.title;
   const expenseAmount = props.amount;
+
+
+
+
+
+  const clickHandler = () => {
+    setTitle('Updated!!');
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
